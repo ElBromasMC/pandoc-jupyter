42882f646a61ac7a0000cb0b3a69b883841b0d1a
Ander Linares <ander.linares@outlook.com>
2024-09-22 16:05:07 -0500

    fix interpolation error bound

--- src/interpolation/error.py
+++ src/interpolation/error.py
@@ -2,6 +2,19 @@ import numpy as np
 import sympy as sp
 import math
 
+def m_error_bound(f_sym, n, a, b):
+    x_sym = sp.symbols('x')
+
+    # Calculamos la n derivada de la función
+    ndf = sp.lambdify(x_sym, sp.diff(f_sym, x_sym, n), "numpy")
+
+    I = np.linspace(a, b, 1000)
+    
+    # Calculamos la cota de error
+    M = np.max(np.abs(ndf(I)))
+    
+    return M
+
 # f_sym: Es una función simbólica de variable 'x' que representa la función a aproximar
 # nodes: Lista que contiene los nodos de la interpolación
 # xs: Lista de puntos donde evaluar el error
@@ -10,13 +23,8 @@ def general_error_bound(f_sym, nodes, xs):
     n = len(nodes)
     a = min(nodes)
     b = max(nodes)
-    x_sym = sp.symbols('x')
 
-    # Calculamos la n derivada de la función
-    nth_derivative = sp.diff(f_sym, x_sym, n)
-
-    # Calculamos las cota de error
-    M = sp.maximum(nth_derivative, x_sym, sp.Interval(a, b)).evalf()
+    M = m_error_bound(f_sym, n, a, b)
     error_bound = []
     for elem in xs:
         P = 1
@@ -30,26 +38,8 @@ def general_error_bound(f_sym, nodes, xs):
 # a, b: Intervalo de la interpolación
 # error_bound: La cota del error para una interpolación con nodos igualmente espaciados
 def evenly_error_bound(f_sym, n, a, b):
-    x_sym = sp.symbols('x')
-
-    # Calculamos la n derivada de la función
-    nth_derivative = sp.diff(f_sym, x_sym, n)
-    
-    # Calculamos la cota de error
-    M = sp.maximum(nth_derivative, x_sym, sp.Interval(a, b)).evalf()
+    M = m_error_bound(f_sym, n, a, b)
     h = (b-a)/(n-1)
     error_bound = h**n*M/(4*n)
     
     return error_bound
-
-def m_error_bound(f_sym, n, a, b):
-    x_sym = sp.symbols('x')
-
-    # Calculamos la n derivada de la función
-    nth_derivative = sp.diff(f_sym, x_sym, n)
-    
-    # Calculamos la cota de error
-    M = sp.maximum(nth_derivative, x_sym, sp.Interval(a, b)).evalf()
-    
-    return M
-
